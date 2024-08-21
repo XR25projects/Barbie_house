4145541a5842ab65d42e65d90200e76d8900abc9
MaryAusht <ausht.mary@gmail.com>
2024-08-21 12:51:36 +0200

    added animation on barbie house

---
+++ Assets/Scripts/videoGameAudio.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class videoGameAudio : MonoBehaviour
+{
+    public AudioSource audioSource;  // Reference to the AudioSource component
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the player is the one entering the trigger
+        if (other.CompareTag("Player"))
+        {
+            // Play the audio
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
